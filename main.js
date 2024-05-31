4ea1f028006dd742b14516763e9f92d0486e699e
micymike <mosesmichael878@gmail.com>
2024-05-31 20:55:54 +0300

    4th

--- main.js
+++ main.js
@@ -20,12 +20,3 @@ document.getElementById("sign-el").addEventListener("click", function(){
     }
 });
 
-document.getElementById('sign-up-form').addEventListener('submit', function(event) {
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
-    
-    if (password !== confirmPassword) {
-        event.preventDefault();
-        alert('Passwords do not match.');
-    }
-});
