c2faa71329106121bc1eda2775486741030c8662
micymike <mosesmichael878@gmail.com>
2024-06-02 10:14:05 +0300

    "Updated HTML files (About, Home, Contact, Index, Login) and JavaScript file (main.js) with changes to content, formatting, and icon references."

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 document.getElementById('sign-up-form').addEventListener('submit', function(event) {
+    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirm-password').value;
-    
-    if (password !== confirmPassword) {
+
+    if (email === '' || password === '') {
+        event.preventDefault();
+        alert('Please fill in all fields.');
+    } else if (password !== confirmPassword) {
         event.preventDefault();
         alert('Passwords do not match.');
-    }
-});
-
-document.getElementById("sign-el").addEventListener("click", function(){
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
-    
-    if (password === confirmPassword && password !== "") {
-        document.getElementById("sign-up-form").submit();
-        window.location.href = 'home.html';
     } else {
-        alert('Passwords do not match.');
+        // Redirect to home.html after form submission
+        event.preventDefault(); // Prevent actual form submission
+        window.location.href = 'home.html';
     }
 });
 
+document.getElementById('log-el').addEventListener('click', function(event) {
+    const form = document.getElementById('sign-up-form');
+    form.submit();  // Trigger form submission
+});
